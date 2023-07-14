39e1856ab336840fc2555fb36dc688de1eb399e7
Nes0x <przemek882@onet.pl>
2023-07-14 14:46:14 +0200

    edit vision of language model

--- 7tools/Models/Language.cs
+++ 7tools/Models/Language.cs
@@ -5,8 +5,8 @@ public class Language
     public required string Name { get; init; }
     public required float InstalledVersion { get; init; }
     public required float CurrentVersion { get; init; }
-    public required string PathToImage { get; init; }
-    public required string UrlToApi { get; init; }
+    public required string ImageUrl { get; init; }
+    public required string[] DownloadUrls { get; set; }
     public string DownloadPath { get; set; }
     public bool IsChecked { get; set; }
     public bool ShouldBeEnvironmentVariable { get; set; }
