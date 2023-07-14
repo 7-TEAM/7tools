fc08fa0d84f29107278cb8db9409ad42c7dbdedc
Nes0x <przemek882@onet.pl>
2023-07-14 14:33:16 +0200

    add a language model

---
+++ 7tools/Models/Language.cs
@@ -0,0 +1,14 @@
+﻿namespace Tools.Models;
+
+public class Language
+{
+    public required string Name { get; init; }
+    public required float InstalledVersion { get; init; }
+    public required float CurrentVersion { get; init; }
+    public required string PathToImage { get; init; }
+    public required string UrlToApi { get; init; }
+    public string DownloadPath { get; set; }
+    public bool IsChecked { get; set; }
+    public bool ShouldBeEnvironmentVariable { get; set; }
+    
+}
