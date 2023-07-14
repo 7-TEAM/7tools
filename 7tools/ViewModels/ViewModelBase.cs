1a4f4168d37828474adfb1aaeb6be9d1dc2b0625
Nes0x <przemek882@onet.pl>
2023-07-14 12:18:17 +0200

    init

---
+++ 7tools/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Tools.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ 7tools/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Tools.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
