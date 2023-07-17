57f158fba2ebbdf94948b7679da5fbfdec40ba80
Nes0x <przemek882@onet.pl>
2023-07-17 15:42:44 +0200

    Change code of method UpdateLanguages

--- 7tools.View/ViewModels/MainWindowViewModel.cs
+++ 7tools.View/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
-using System.Threading;
 using System;
+using System.Threading.Tasks;
 using SvTools.Models;
+using SvTools.Models.Extensions;
 using SvTools.Services;
 using SvTools.Services.DataAccess;
 using SvTools.Services.WebAccess;
-using SvTools.Models.Extensions;
+using Timer = System.Timers.Timer;
 
 namespace SvTools.View.ViewModels;
 
@@ -18,41 +19,42 @@ public class MainWindowViewModel : ViewModelBase
     public bool IsDownloadingButtonPressed {get; set;}
 
     private const string FileName = "config.json";
+    private readonly LanguageService _languageService;
+    private readonly DownloadService _downloadService;
 
     public MainWindowViewModel()
     {
         Languages = new List<Language>();
         LanguageButtons = new List<LanguageButton>();
-
-        new Thread(UpdateLanguages).Start();
-    }
-
-    public async void UpdateLanguages()
-    {
         var fileService = new FileService();
         var httpService = new HttpService(new HttpClient());
-        var languageService = new LanguageService(fileService, httpService, FileName);
-        
-        while (true){
-            Thread.Sleep(5000);
+        _languageService = new LanguageService(fileService, httpService, FileName);
+        _downloadService = new DownloadService(httpService);
+        UpdateLanguages();
+    }
+
+    private void UpdateLanguages()
+    {
+        var timer = new Timer(5000);
+        async Task UpdateTimerAsync()
+        {
             try
             {
                 Languages = new List<Language>(
-                    await languageService.GetLanguagesAsync(
+                    await _languageService.GetLanguagesAsync(
                         $"api/languages?platform={RuntimeInformationExtensions.PlatformName()}"
                     )
                 );
 
-                Languages.ForEach((element) => LanguageButtons.Add(new LanguageButton(element)));
+                Languages.ForEach(language => LanguageButtons.Add(new LanguageButton(language)));
             }
             catch (Exception)
             {
 
             }
+             
         }
-    }
-
-    private void Download(){
-
+        timer.Elapsed += (_, _) => UpdateTimerAsync().GetAwaiter().GetResult();
+        timer.Enabled = true;
     }
 }
